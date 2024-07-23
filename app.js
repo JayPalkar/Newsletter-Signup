7df108896a89f3673517258ca212437fbf1a2d09
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-07-23 16:54:12 +0530

    posting data to mailchimp

--- app.js
+++ app.js
@@ -12,11 +12,53 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    console.log(req.body.Fname);
-    console.log(req.body.Lname);
-    console.log(req.body.Email);
-})
+    var FirstName = req.body.Lname
+    var LastName= req.body.Email
+    var Email = req.body.Fname
+
+
+    var data = {
+        members:[
+            {
+                email_address: Email,
+                status: "subscribed"
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    var option = {
+        url: "https://us13.api.mailchimp.com/3.0/lists/bd565f3834",
+        method: "POST",
+        headers: {
+            "Authorization": "Jay1 a42e0501afcc2b4f3e7448ce980a9c59-us13"
+        },
+        body: jsonData
+    }
+
+    request(option, function(error, response, body){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(response.statusCode);
+        }
+    });
+
+});
+
+
+
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000 ....!");
 });
+
+
+
+// API KEY
+// a42e0501afcc2b4f3e7448ce980a9c59-us13
+
+// List ID
+// bd565f3834
